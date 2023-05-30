13d5d98d725a570b3487942f990512fed588ed67
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-05-30 11:41:41 +0300

    Add accessory manager

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,3 +1,3 @@
+const Accessory = require('../models/Acccessory');
 
-
-exports.create = (accessoryData) => console.log(accessoryData);
+exports.create = (accessoryData) => Accessory.create(accessoryData);
