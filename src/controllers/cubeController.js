4e1c48a987a09a6cc85f2c4884cba840afb62a4c
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-05-30 11:58:30 +0300

    Add attach accessory page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -35,4 +35,8 @@ router.get('/:cubeId/details', async(req, res) => {
   res.render('details', { cube });
 });
 
+router.get('/:cubeId/attach-accessory', (req, res) => {
+   res.render('accessory/attach');
+});
+
 module.exports = router;
