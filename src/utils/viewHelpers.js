97b2d9d61aa06abedbb60daf96a4acf3b7058cd3
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-06-06 13:19:47 +0300

    Generate options for difficulty level

---
+++ src/utils/viewHelpers.js
@@ -0,0 +1,18 @@
+exports.getDifficultyOptionsViewData = function (difficultyLevel) {
+  const titles = [
+    'Very Easy',
+    'Easy',
+    'Medium (Standart 3x3)',
+    'Intermediate',
+    'Expert',
+    'Hardcore',
+  ];
+
+  const options = titles.map((title, index) => ({
+    title: `${index + 1} - ${title}`,
+    value: index + 1,
+    selected: Number(difficultyLevel) === index + 1
+  }));
+
+  return options;
+};
