028d51344a730a4148bd0c66009dd531e065153f
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-06-06 12:15:57 +0300

    Add delete cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const accessoryManager = require('../managers/accessoryManager');
 
 // Path /cubes/create
 router.get("/create", (req, res) => {
-  res.render("create");
+  res.render("cube/create");
 });
 
 router.post("/create", async (req, res) => {
@@ -34,7 +34,7 @@ router.get('/:cubeId/details', async(req, res) => {
     return res.redirect('/404');
   }
 
-  res.render('details', { cube });
+  res.render('cube/details', { cube });
 });
 
 router.get('/:cubeId/attach-accessory', async(req, res) => {
@@ -54,5 +54,11 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 
   res.redirect(`/cubes/${cubeId}/details`);
 });
+
+router.get('/:cubeId/delete', async (req, res) => {
+  const cube = await cubeManager.getOne(req.params.cubeId).lean();
+
+  res.render('cube/delete', { cube });
+})
 
 module.exports = router;
